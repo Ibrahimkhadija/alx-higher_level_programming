30d30a041faef1e3b9f46235bdc678d2e43bff5e
https://github.com/Ibrahimkhadija/alx_higher_level-programming.git <mahmudkhadija@gmail.com>
2024-03-11 04:16:58 -0700

    0x01-python-if_else_loops_functions

--- 102-magic_calculation.py
+++ 102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 def magic_calculation(a, b):
     return (98 + a ** b)
+=======
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
+>>>>>>> 0x01-python-if_else_loops_functions
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 Python - Hello, World
+=======
+0x01-python-if_else_loops_functions
+>>>>>>> 0x01-python-if_else_loops_functions
